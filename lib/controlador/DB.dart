7e5613928f3b7b11182fcaf25220163fe1636163
Gespinosa <gespinosa01@icloud.com>
2024-04-19 10:53:30 -0700

    Creacion de las bases de datos mejorado

--- lib/controlador/DB.dart
+++ lib/controlador/DB.dart
@@ -5,49 +5,52 @@ class Conexion {
   static Database? _database;
 
   static Future<Database> get database async {
-    if(_database != null) return _database!;
+    if (_database != null) return _database!;
     _database = await _openDB();
     return _database!;
   }
 
   static Future<Database> _openDB() async {
     return openDatabase(join(await getDatabasesPath(), 'checador.db'),
-      onCreate: (db, version){
+        onCreate: (db, version) {
       return script(db);
-      }, version: 1);
+    }, version: 1);
   }
 
-  static Future<void> script(db) async{
-    db.execute('''
-    CREATE TABLE MATERIA(
-      NMAT TEXT PRIMARY KEY,
-      DESCRIPCION TEXT
-    )
+  static Future<void> script(Database db) async {
+    await db.execute('''
+      CREATE TABLE MATERIA(
+        NMAT TEXT PRIMARY KEY,
+        DESCRIPCION TEXT
+      );
     ''');
-    db.execute('''
-    CREATE TABLE PROFESOR(
-      NPROFESOR TEXT,
-      NOMBRE TEXT,
-      CARRERA TEXT
-    )
+    await db.execute('''
+      CREATE TABLE PROFESOR(
+        NPROFESOR TEXT,
+        NOMBRE TEXT,
+        CARRERA TEXT
+      );
     ''');
-    db.execute('''
-    CREATE TABLE HORARIO(
-      NHORARIO INT PRIMARY KEY AUTOINCREMENT,
-      NPROFESOR TEXT FOREIGN KEY,
-      NMAT TEXT FOREIGN KEY,
-      HORA TEXT,
-      EDIFICIO TEXT,
-      SALON TEXT
-    )
+    await db.execute('''
+      CREATE TABLE HORARIO(
+        NHORARIO INTEGER PRIMARY KEY AUTOINCREMENT,
+        NPROFESOR TEXT,
+        NMAT TEXT,
+        HORA TEXT,
+        EDIFICIO TEXT,
+        SALON TEXT,
+        FOREIGN KEY(NPROFESOR) REFERENCES PROFESOR(NPROFESOR),
+        FOREIGN KEY(NMAT) REFERENCES MATERIA(NMAT)
+      );
     ''');
-    db.execute('''
-    CREATE TABLE ASISTENCIA(
-      IDASISTENCIA INT PRIMARY KEY,
-      NHORARIO INT FOREIGN KEY,
-      FECHA TEXT,
-      ASISTENCIA BOOLEAN
-    )
+    await db.execute('''
+      CREATE TABLE ASISTENCIA(
+        IDASISTENCIA INTEGER PRIMARY KEY,
+        NHORARIO INTEGER,
+        FECHA TEXT,
+        ASISTENCIA BOOLEAN,
+        FOREIGN KEY(NHORARIO) REFERENCES HORARIO(NHORARIO)
+      );
     ''');
   }
 }
