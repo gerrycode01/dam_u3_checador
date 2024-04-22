f8d56f0e215da4e13d49c1c657f22f88d1ef95da
Gespinosa <gespinosa01@icloud.com>
2024-04-22 11:28:45 -0700

    Query1, retornando datos

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:dam_u3_practica1_checador/query1.dart';
 import 'package:dam_u3_practica1_checador/controlador/DB.dart';
 import 'package:dam_u3_practica1_checador/controlador/DBProfesor.dart';
 import 'package:dam_u3_practica1_checador/modelo/profesor.dart';
+import 'package:dam_u3_practica1_checador/query2.dart';
 import 'package:dam_u3_practica1_checador/vista/asistencias/VistaAsistencia.dart';
 import 'package:dam_u3_practica1_checador/vista/horarios/VistaHorario.dart';
 import 'package:dam_u3_practica1_checador/vista/materias/VistaMateria.dart';
@@ -29,7 +31,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _currentIndex = 0;// Corrected variable name for consistency
+  int _currentIndex = 0; // Corrected variable name for consistency
   final Color azulMarino = Colors.indigo.shade900;
   final Color naranja = Colors.deepOrange;
   final Color blanco = Colors.white;
@@ -48,12 +50,14 @@ class _MyHomePageState extends State<MyHomePage> {
       _currentIndex = index;
     });
   }
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     cargarLista();
   }
+
   void cargarLista() async {
     List<Profesor> l = await DBProfesor.mostrar();
     setState(() {
@@ -83,7 +87,8 @@ class _MyHomePageState extends State<MyHomePage> {
             const DrawerHeader(
               decoration: BoxDecoration(
                 image: DecorationImage(
-                  image: AssetImage('assets/fondo.jpeg'), // Asegúrate de cambiar esto por el path de tu imagen.
+                  image: AssetImage('assets/fondo.jpeg'),
+                  // Asegúrate de cambiar esto por el path de tu imagen.
                   fit: BoxFit.cover,
                 ),
               ),
@@ -95,36 +100,68 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
               ),
             ),
-
             ListTile(
               leading: const Icon(Icons.person),
-              title: const Text('Profesores', style: TextStyle(color: Colors.deepOrange,fontWeight: FontWeight.bold,),),
+              title: const Text(
+                'Profesores',
+                style: TextStyle(
+                  color: Colors.deepOrange,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
               onTap: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => const VistaProfesor()));
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const VistaProfesor()));
               },
             ),
             ListTile(
               leading: const Icon(Icons.book),
-              title: const Text('Materias', style: TextStyle(color: Colors.deepOrange,fontWeight: FontWeight.bold,),),
+              title: const Text(
+                'Materias',
+                style: TextStyle(
+                  color: Colors.deepOrange,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
               onTap: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => const VistaMateria()));
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const VistaMateria()));
               },
             ),
             ListTile(
               leading: const Icon(Icons.schedule),
-              title: const Text('Horarios', style: TextStyle(color: Colors.deepOrange,fontWeight: FontWeight.bold,),),
+              title: const Text(
+                'Horarios',
+                style: TextStyle(
+                  color: Colors.deepOrange,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
               onTap: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => const Horarios()));
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => const Horarios()));
               },
             ),
             ListTile(
               leading: const Icon(Icons.check_circle),
-              title: const Text('Asistencias', style: TextStyle(color: Colors.deepOrange,fontWeight: FontWeight.bold,),),
+              title: const Text(
+                'Asistencias',
+                style: TextStyle(
+                  color: Colors.deepOrange,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
               onTap: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => const VistaAsistencia()));
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const VistaAsistencia()));
               },
             ),
-
           ],
         ),
       ),
@@ -154,91 +191,17 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget _dynamicContent() {
     switch (_currentIndex) {
       case 1:
-        return Padding(
-          padding: EdgeInsets.all(10),
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  Expanded(
-                    child: DropdownButtonFormField<String>(
-                      decoration: InputDecoration(
-                        labelText: "Profesor",
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        prefixIcon: Icon(Icons.person, color: azulMarino),
-                        filled: true,
-                        fillColor: blanco,
-                      ),
-                      value: idProfesor,
-                      onChanged: (String? newValue) {
-                        setState(() {
-                          idProfesor = newValue;
-                        });
-                      },
-                      items: profesores.map<DropdownMenuItem<String>>((Profesor profesor) {
-                        return DropdownMenuItem<String>(
-                          value: profesor.nprofesor,
-                          child: Text(profesor.nombre),
-                        );
-                      }).toList(),
-                    ),
-                  ),
-                  SizedBox(width: 10),
-                  Expanded(
-                    child: TextField(
-                      controller: _fechaController,
-                      decoration: InputDecoration(
-                        hintText: 'Fecha',
-                        prefixIcon: Icon(Icons.calendar_today, color: azulMarino),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        filled: true,
-                        fillColor: blanco,
-                      ),
-                      readOnly: true,
-                      onTap: () async {
-                        DateTime? pickedDate = await showDatePicker(
-                          context: context,
-                          initialDate: DateTime.now(),
-                          firstDate: DateTime(2000),
-                          lastDate: DateTime(2100),
-                        );
-                        if (pickedDate != null) {
-                          _fechaController.text = pickedDate.toString().substring(0, 10); // Formatea la fecha como yyyy-mm-dd
-                        }
-                      },
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  foregroundColor: blanco, backgroundColor: naranja,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-                onPressed: () {
-                  // Agregar lógica para la acción de buscar aquí
-                },
-                child: const Text('Buscar'),
-              ),
-            ],
-          ),
-        );
+        return Query2();
       case 2:
         return Padding(
-          padding: EdgeInsets.all(10),
+          padding: const EdgeInsets.all(10),
           child: Column(
             children: [
               Row(
                 children: [
                   Expanded(
-                    flex: 5, // Esto da más espacio al dropdown en proporción al botón
+                    flex: 5,
+                    // Esto da más espacio al dropdown en proporción al botón
                     child: DropdownButtonFormField<String>(
                       decoration: InputDecoration(
                         labelText: "Profesor",
@@ -255,7 +218,8 @@ class _MyHomePageState extends State<MyHomePage> {
                           idProfesor = newValue;
                         });
                       },
-                      items: profesores.map<DropdownMenuItem<String>>((Profesor profesor) {
+                      items: profesores
+                          .map<DropdownMenuItem<String>>((Profesor profesor) {
                         return DropdownMenuItem<String>(
                           value: profesor.nprofesor,
                           child: Text(profesor.nombre),
@@ -263,13 +227,17 @@ class _MyHomePageState extends State<MyHomePage> {
                       }).toList(),
                     ),
                   ),
-                  SizedBox(width: 10), // Espacio entre el dropdown y el botón
+                  const SizedBox(width: 10),
+                  // Espacio entre el dropdown y el botón
                   ButtonTheme(
-                    minWidth: 64.0, // Ancho mínimo del botón
-                    height: 60.0, // Altura para igualar la del DropdownButtonFormField
+                    minWidth: 64.0,
+                    // Ancho mínimo del botón
+                    height: 60.0,
+                    // Altura para igualar la del DropdownButtonFormField
                     child: ElevatedButton(
                       style: ElevatedButton.styleFrom(
-                        foregroundColor: blanco, backgroundColor: naranja,
+                        foregroundColor: blanco,
+                        backgroundColor: naranja,
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(10),
                         ),
@@ -286,83 +254,7 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         );
       default:
-        return Padding(
-          padding: EdgeInsets.all(10),
-           child: (
-             Column(
-               children: [
-                 Row(
-                   children: [
-                     Expanded(
-                       child: DropdownButtonFormField<String>(
-                         decoration: InputDecoration(
-                           labelText: "Hora",
-                           border: OutlineInputBorder(
-                             borderRadius: BorderRadius.circular(10),
-                           ),
-                           prefixIcon: Icon(Icons.punch_clock, color: azulMarino),
-                           filled: true,
-                           fillColor: blanco,
-                         ),
-                         onChanged: (newValue) {
-                           setState(() {
-                             selectedTime = newValue;
-                           });
-                         },
-                         items: Conexion.horas.map<DropdownMenuItem<String>>((String value) {
-                           return DropdownMenuItem<String>(
-                             value: value,
-                             child: Text(value),
-                           );
-                         }).toList(),
-                       ),
-                     ),
-                     SizedBox(width: 10),
-                     Expanded(
-                       child: DropdownButtonFormField<String>(
-                         decoration: InputDecoration(
-                           labelText: "Edificio",
-                           border: OutlineInputBorder(
-                             borderRadius: BorderRadius.circular(10),
-                           ),
-                           prefixIcon: Icon(Icons.apartment, color: azulMarino),
-                           filled: true,
-                           fillColor: blanco,
-                         ),
-                         value: selectedEdificio,
-                         onChanged: (newValue) {
-                           setState(() {
-                             selectedEdificio = newValue;
-                           });
-                         },
-                         items: Conexion.edificiosYSalones.keys
-                             .map<DropdownMenuItem<String>>((String value) {
-                           return DropdownMenuItem<String>(
-                             value: value,
-                             child: Text(value),
-                           );
-                         }).toList(),
-                       ),
-                     ),
-                   ],
-                 ),
-                 SizedBox(height: 20),
-                 ElevatedButton(
-                   style: ElevatedButton.styleFrom(
-                     foregroundColor: blanco, backgroundColor: naranja,
-                     shape: RoundedRectangleBorder(
-                       borderRadius: BorderRadius.circular(10),
-                     ),
-                   ),
-                   onPressed: () {
-                     // Añadir lógica de búsqueda aquí si es necesario
-                   },
-                   child: const Text('Buscar'),
-                 ),
-               ],
-             )
-           ),
-        );
+        return const Query1();
     }
   }
 }
---
+++ lib/query1.dart
@@ -0,0 +1,119 @@
+import 'package:dam_u3_practica1_checador/controlador/DB.dart';
+import 'package:dam_u3_practica1_checador/controlador/DBProfesor.dart';
+import 'package:dam_u3_practica1_checador/modelo/profesorHorario.dart';
+import 'package:flutter/material.dart';
+
+class Query1 extends StatefulWidget {
+  const Query1({super.key});
+
+  @override
+  State<Query1> createState() => _Query1State();
+}
+
+class _Query1State extends State<Query1> {
+  final Color azulMarino = Colors.indigo.shade900;
+  final Color naranja = Colors.deepOrange;
+  final Color blanco = Colors.white;
+  final Color negro = Colors.black;
+  List<ProfesorHorario> profesores = [];
+  String? selectedTime;
+  String? selectedEdificio;
+  String? selectedSalon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: (Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: DropdownButtonFormField<String>(
+                  decoration: InputDecoration(
+                    labelText: "Hora",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    prefixIcon: Icon(Icons.punch_clock, color: azulMarino),
+                    filled: true,
+                    fillColor: blanco,
+                  ),
+                  onChanged: (newValue) {
+                    setState(() {
+                      selectedTime = newValue;
+                    });
+                  },
+                  items: Conexion.horas
+                      .map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                ),
+              ),
+              const SizedBox(width: 10),
+              Expanded(
+                child: DropdownButtonFormField<String>(
+                  decoration: InputDecoration(
+                    labelText: "Edificio",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    prefixIcon: Icon(Icons.apartment, color: azulMarino),
+                    filled: true,
+                    fillColor: blanco,
+                  ),
+                  value: selectedEdificio,
+                  onChanged: (newValue) {
+                    setState(() {
+                      selectedEdificio = newValue;
+                    });
+                  },
+                  items: Conexion.edificiosYSalones.keys
+                      .map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              foregroundColor: blanco,
+              backgroundColor: naranja,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            onPressed: () {
+              cargarProfesores();
+              // Añadir lógica de búsqueda aquí si es necesario
+            },
+            child: const Text('Buscar'),
+          ),
+        ],
+      )),
+    );
+  }
+
+  void cargarProfesores() async {
+    if (selectedTime == null || selectedEdificio == null) return;
+
+    List<ProfesorHorario> profesores = await DBProfesor.query1(
+        selectedTime.toString(), selectedEdificio.toString());
+
+    for (var element in profesores) {
+      print(element.nombre);
+    }
+    setState(() {
+      this.profesores = profesores;
+      //AQUI ESCRIBES O ACTUALIZAS LO QUE TENGAS QUE ACTUALIZAR
+    });
+  }
+}
